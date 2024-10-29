d7d3ac367c9d90f56772ce4e5e10a97a6fb08fa1
bluefits <44682470+BlueFits@users.noreply.github.com>
2024-10-29 16:53:26 -0400

    updated form

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import FormControl from '@mui/material/FormControl';
 import IconButton from '@mui/material/IconButton';
 import { animated, useSpring } from '@react-spring/web'
 import { navigate } from "gatsby"
@@ -38,24 +39,27 @@ const SearchInput: React.FC<ISearchInput> = ({ disableAnim } = { disableAnim: fa
     }
 
     const handleNavigation = () => navigate(`/search?avg=${nagivationRef.current}`);
-
-    const searchKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter") submit();
-    }
     
     return (
         <animated.div style={props} className='p-[10px] rounded-[15px] bg-[#F8F8F8] flex justify-start items-center border-[none] w-full'>
-            <IconButton onClick={submit}>
+            {/* <IconButton onClick={submit}>
+                <SearchRoundedIcon />
+            </IconButton> */}
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                submit();
+            }}>
+            <IconButton type='submit'>
                 <SearchRoundedIcon />
             </IconButton>
-            <input 
-                onChange={(e) => setSearch(e.target.value)}
-                value={search}
-                onKeyDown={searchKeyDownHandler} 
-                className='bg-[#F8F8F8] outline-none border-[none]' 
-                type="text" 
-                placeholder='Enter your average' 
-            />
+                <input 
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                    className='bg-[#F8F8F8] outline-none border-[none]' 
+                    type="text" 
+                    placeholder='Enter your average' 
+                />
+            </form>
         </animated.div>
     );
 };
