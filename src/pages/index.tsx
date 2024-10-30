0627c91a495dbc317a70d859acf122b6e0b86443
bluefits <44682470+BlueFits@users.noreply.github.com>
2024-10-30 13:59:36 -0400

    added version number

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import SearchInput from "../components/SearchInput/SearchInput";
+import { Typography } from "@mui/material";
 
 
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen flex-col">
       <div className="w-4/5">
         <SearchInput />
       </div>
+      <Typography className="mt-5 text-[gray]" variant="caption">SU v1.0.0</Typography>
     </div>
   )
 }
