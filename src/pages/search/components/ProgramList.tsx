d0851e89914378a1e3bfe34f3ca74a1729beb5e2
bluefits <44682470+BlueFits@users.noreply.github.com>
2024-10-30 23:33:41 -0400

    added new data file

--- src/pages/search/components/ProgramList.tsx
+++ src/pages/search/components/ProgramList.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Typography from "@mui/material/Typography/Typography";
 import { program } from "../../../services/modules/programs/programs.slice";
 
+
 interface IProgramList {
     program: program | null;
 }
@@ -10,14 +11,16 @@ const ProgramList: React.FC<IProgramList> = ({ program }) => {
 
     return program ? (
         <li className="last:mb-0 mb-4 border-[1px] border-[solid] border-[#E7E7E7] rounded-[15px] p-5 flex justify-center items-center">
-            <div className="flex-[3]">
-                <Typography className="mb-1" variant="body1">{program.programName}</Typography>
-                <Typography variant="caption">{program.schoolName}</Typography>
-            </div>
+            <a className="w-full flex justify-center items-center" href={program.programLink} target="_blank">
+                <div className="flex-[3]">
+                    <Typography className="mb-1" variant="body1">{program.programName}</Typography>
+                    <Typography variant="caption">{program.schoolName}</Typography>
+                </div>
 
-            <div className="flex-[1] flex items-center justify-end">
-                <Typography className="text-[#0D47A1]" variant="body1">{program.entranceGrade}</Typography>
-            </div>
+                <div className="flex-[1] flex items-center justify-end">
+                    <Typography className="text-[#0D47A1]" variant="body1">{program.entranceGrade}</Typography>
+                </div>
+            </a>
         </li>
     ) : <></>;
 }
