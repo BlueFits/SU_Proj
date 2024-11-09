c37398b1bff90da0bc27380ae57b2c37057ac413
bluefits <44682470+BlueFits@users.noreply.github.com>
2024-11-09 10:15:38 -0500

    added analytics

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,24 +10,44 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
-    "gatsby-plugin-postcss", 
-    /*"gatsby-plugin-google-gtag",*/ 
-    "gatsby-plugin-image", 
-    "gatsby-plugin-sitemap", 
-    "gatsby-plugin-layout", 
+    "gatsby-plugin-postcss",
+    // "gatsby-plugin-google-gtag",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-layout",
     {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-W2YP40HSXZ", // Google Analytics / GA tracking ID
+        ],
+        // This object is optional and will configure other tracking settings.
+        gtagConfig: {
+          anonymize_ip: true, // Sets IP anonymization
+        },
+        pluginConfig: {
+          head: true, // Puts tracking script in the <head> instead of the <body>
+          respectDNT: true, // Respects "Do Not Track" settings
+        },
+      },
+    }
+  ]
 };
 
 export default config;
