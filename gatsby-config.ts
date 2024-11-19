fc14975c168d2c6d350de8bc3f931bd6228645b1
bluefits <44682470+BlueFits@users.noreply.github.com>
2024-11-18 21:36:23 -0500

    added manifest

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,8 +2,9 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `SU_proj`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `SelectU`,
+    description: `Find an undergrad program based on your average`,
+    siteUrl: `https://www.selectu.org`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
